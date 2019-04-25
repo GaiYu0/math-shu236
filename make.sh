c4be1e1dad55ed8ae58dcc94e6299df0b98a8c65
Yu Gai <yg1246@nyu.edu>
2019-04-25 10:41:27 +0800

    commit

---
+++ make.sh
@@ -0,0 +1,6 @@
+file=written
+pdflatex -interaction=nonstopmode $file.tex
+bibtex $file.aux
+pdflatex -interaction=nonstopmode $file.tex
+pdflatex -interaction=nonstopmode $file.tex
+xdg-open $file.pdf
